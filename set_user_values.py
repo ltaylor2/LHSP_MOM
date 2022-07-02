697fa0f2dcaaae167a97b02c95a3ad1c5c544c4d
ninthgiant <mauckr@kenyon.edu>
2022-07-02 17:43:13 -0400

    Update set_user_values.py
    Minor change in the default values for the known weights for calibration

--- set_user_values.py
+++ set_user_values.py
@@ -3,7 +3,8 @@
 #
 
 # Calibration values, in grams
-cal1_value = 27.7
+# cal1_value = 27.7  # we no longer use this calibration weight. Changed to proper weight by RAM, July 2, 2022
+cal1_value = 32.6
 cal2_value = 50.0
 cal3_value = 65.3
 
